96c9752f21de602a3bd32498e9c3fc454befddeb
tre <tremarkley@gmail.com>
2025-07-08 16:13:02 -0700

    feat(experiment): update GasTank

--- src/experiment/IGasTank.sol
+++ src/experiment/IGasTank.sol
@@ -23,47 +23,60 @@ interface IGasTank {
     );
     event Deposit(address indexed gasProvider, uint256 amount);
     event RelayedMessageGasReceipt(
-        bytes32 indexed messageHash, address indexed relayer, uint256 relayCost, bytes32[] nestedMessageHashes
+        bytes32 indexed messageHash,
+        address indexed relayer,
+        address gasProvider,
+        uint256 gasProviderChainID,
+        uint256 relayCost,
+        bytes32[] nestedMessageHashes
     );
     event WithdrawalInitiated(address indexed from, uint256 amount);
     event WithdrawalFinalized(address indexed from, address indexed to, uint256 amount);
 
     // Errors
-    error MaxDepositExceeded();
+    error AlreadyClaimed();
     error InvalidOrigin();
     error InvalidPayload();
     error InsufficientBalance();
-    error AlreadyClaimed();
     error MessageNotAuthorized();
     error WithdrawPending();
     error InvalidLength();
+    error InvalidChainID();
 
     // Constants
-    function MAX_DEPOSIT() external pure returns (uint256);
     function WITHDRAWAL_DELAY() external pure returns (uint256);
     function MESSENGER() external pure returns (IL2ToL2CrossDomainMessenger);
 
     // State Variables
     function balanceOf(address) external view returns (uint256);
     function withdrawals(address) external view returns (uint256 timestamp, uint256 amount);
-    function claimed(bytes32) external view returns (bool);
     function authorizedMessages(address, bytes32) external view returns (bool);
 
     // Functions
     function deposit(address _to) external payable;
     function initiateWithdrawal(uint256 _amount) external;
     function finalizeWithdrawal(address _to) external;
-    function authorizeClaim(bytes32 _messageHash) external;
+    function authorizeClaim(bytes32[] calldata _messageHashes) external;
     function relayMessage(
         Identifier calldata _id,
-        bytes calldata _sentMessage
-    )
-        external
-        returns (uint256 relayCost_, bytes32[] memory nestedMessageHashes_);
-    function claim(Identifier calldata _id, address _gasProvider, bytes calldata _payload) external;
+        bytes calldata _sentMessage,
+        address _gasProvider,
+        uint256 _gasProviderChainID
+    ) external returns (uint256 relayCost_, bytes32[] memory nestedMessageHashes_);
+    function claim(Identifier calldata _id, bytes calldata _payload) external;
     function decodeGasReceiptPayload(bytes calldata _payload)
         external
         pure
-        returns (bytes32 messageHash_, address relayer_, uint256 relayCost_, bytes32[] memory nestedMessageHashes_);
-    function claimOverhead(uint256 _numHashes, uint256 _baseFee) external pure returns (uint256);
+        returns (
+            bytes32 messageHash_,
+            address relayer_,
+            address gasProvider_,
+            uint256 gasProviderChainID_,
+            uint256 relayCost_,
+            bytes32[] memory nestedMessageHashes_
+        );
+    function claimOverhead(uint256 _numHashes, uint256 _baseFee, bytes calldata _data)
+        external
+        view
+        returns (uint256 overhead_);
 }
